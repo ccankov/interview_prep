5c872f4ba8060e0ac3b582b757e02be4e48b004c
Chris Cankov <cankov.chris@gmail.com>
2017-07-09 20:23:06 -0700

    Initial commit; complete is_unique

---
+++ arrays_strings/is_unique.rb
@@ -0,0 +1,18 @@
+#   Is Unique:
+#   Implement an algorithm to determine if a string has all unique characters.
+#   What if you cannot use additional data structures?
+
+class String
+  def unique?
+    char_hash = Hash.new { |hash, key| hash[key] = 0 }
+
+    self.each_char do |ch|
+      char_hash[ch] += 1
+    end
+
+    char_hash.values.all? { |val| val == 1 }
+  end
+end
+
+puts "banana".unique?
+puts "ostrich".unique?
